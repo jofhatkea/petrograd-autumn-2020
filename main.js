d6fe735b0a5930e850e4eb6741c8429d6c77a1cb
Jonas Holbech <jofh@kea.dk>
2020-02-05 10:14:35 +0100

    buttons and more card stuff

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ window.addEventListener("load", () => {
 function copyArticle(amount) {
   const article = document.querySelector(".course");
   const sections = document.querySelectorAll("main > section");
+  const classes = ["none", "soldout", "sale", "allergens"];
   for (let i = 0; i < amount; i++) {
     const copy = article.cloneNode(true);
-    sections[Math.floor(Math.random() * sections.length)].appendChild(copy);
+    copy.classList.add(getRandomItem(classes));
+    getRandomItem(sections).appendChild(copy);
   }
   article.remove();
+}
+function getRandomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
