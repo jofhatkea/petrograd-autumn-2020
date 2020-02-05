8423fedae80eb5ba669c1b04ff0dc3df3bd4bdb5
Jonas Holbech <jofh@kea.dk>
2020-02-05 12:48:54 +0100

    done

--- main.js
+++ main.js
@@ -8,7 +8,23 @@ function copyArticle(amount) {
   const classes = ["none", "soldout", "sale", "allergens"];
   for (let i = 0; i < amount; i++) {
     const copy = article.cloneNode(true);
-    copy.classList.add(getRandomItem(classes));
+    const className = getRandomItem(classes);
+    if (className !== "sale") {
+      copy.querySelector(".price-discount").remove();
+    } else {
+      copy.querySelector(".price-discount").textContent = "69,-";
+      copy.querySelector(".price-full").textContent = "39,-";
+    }
+    copy.classList.add(className);
+    const ps = copy.querySelectorAll(".info p");
+    const amountToSave = Math.floor(Math.random() * 3);
+    console.log(amountToSave);
+    for (let i = 2; i >= 0; i--) {
+      if (i > amountToSave) {
+        ps[i].remove();
+      }
+    }
+
     getRandomItem(sections).appendChild(copy);
   }
   article.remove();
