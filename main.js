cc7486193351a09f13a527ddf6c15a158d02b802
Jonas Holbech <jofh@kea.dk>
2020-02-07 12:46:11 +0100

    clone templates

--- main.js
+++ main.js
@@ -8,8 +8,26 @@ fetch("https://kea-alt-del.dk/t5/api/productlist")
   })
 
 function showData(jsonData) {
-  console.log(jsonData)
+  //console.log(jsonData)
+  //[].forEach
+  //{}.somethin
   //1 make a template
 
   //2 loop though jsonData
+  jsonData.forEach(showSingleDish)
+}
+
+function showSingleDish(dish) {
+  console.log(dish)
+  //1 grab the template
+  const template = document.querySelector("#dishTemplate").content;
+
+  //2 make copy
+  const copy = template.cloneNode(true);
+  // change some conetnt
+  copy.querySelector("h3").textContent = dish.name;
+  //append somewhere
+
+  const whoIsYourDaddy = document.querySelector("#starters")
+  whoIsYourDaddy.appendChild(copy)
 }
