7bd2cf935b02a6df584eafae4cf6e7a876294432
Jonas Holbech <jh@jonasholbech.dk>
2020-08-23 12:07:50 +0200

    main.js

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ modal.addEventListener("click", () => {
   modal.classList.add("hide");
 });
 
-fetch("http://kea-alt-del.dk/t5/api/categories")
-  .then(res => res.json())
-  .then(createCategories)
+fetch("https://kea-alt-del.dk/t5/api/categories")
+  .then((res) => res.json())
+  .then(createCategories);
 
 function createCategories(data) {
-  console.log(data)
+  console.log(data);
   data.forEach(function (oneCat) {
     //create links
     const a = document.createElement("a");
@@ -29,23 +29,23 @@ function createCategories(data) {
     section.appendChild(h2);
 
     document.querySelector("main").appendChild(section);
-  })
+  });
   getProducts();
 }
 
 function getProducts() {
   fetch("https://kea-alt-del.dk/t5/api/productlist")
     .then(function (response) {
-      return response.json()
+      return response.json();
     })
     .then(function (data) {
-      showData(data)
-    })
+      showData(data);
+    });
 }
 
 function showData(jsonData) {
-  console.log(jsonData)
-  jsonData.forEach(showSingleDish)
+  console.log(jsonData);
+  jsonData.forEach(showSingleDish);
 }
 
 function showSingleDish(dish) {
@@ -60,56 +60,54 @@ function showSingleDish(dish) {
   copy.querySelector("h3").textContent = dish.name;
   //append somewhere
 
-
-  if (dish.discount) { //on sale
+  if (dish.discount) {
+    //on sale
     copy.querySelector(".price-discount span").textContent = dish.price;
-    const newPrice = Math.round(dish.price - dish.price * dish.discount / 100);
+    const newPrice = Math.round(
+      dish.price - (dish.price * dish.discount) / 100
+    );
 
     copy.querySelector(".price-full span").textContent = newPrice;
     //calculate new price
     //49-49*10/100
     //dish.price-dish.price*price.discount/100
-  } else { // not on discount
-    copy.querySelector(".price-discount").remove()
-    copy.querySelector(".price-full span").textContent = dish.price
+  } else {
+    // not on discount
+    copy.querySelector(".price-discount").remove();
+    copy.querySelector(".price-full span").textContent = dish.price;
   }
 
   copy.querySelector("button").addEventListener("click", () => {
-    console.log("click", dish)
+    console.log("click", dish);
     fetch(`https://kea-alt-del.dk/t5/api/product?id=${dish.id}`)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(showDetails);
   });
 
-  console.log(`#${dish.category}`)
+  console.log(`#${dish.category}`);
   document.querySelector(`#${dish.category}`).appendChild(copy);
 }
 
 function showDetails(data) {
-  console.log(data)
+  console.log(data);
   modal.querySelector(".modal-name").textContent = data.name;
   modal.querySelector(".modal-description").textContent = data.longdescription;
   //...
   modal.classList.remove("hide");
 }
 
-
-
-
-
-
-
 function dumbStuff() {
   const all = document.querySelectorAll("article, article>*");
-  console.log(all)
+  console.log(all);
   setInterval(() => {
     const a = all[Math.floor(Math.random() * all.length)];
     animate(a);
-
-  }, 100)
+  }, 100);
 }
 
 function animate(el) {
-  el.style.transition = 'all 3s';
-  el.style.transform = `translate(${Math.random()*200-100}vw, ${Math.random()*200-100}vh) scale(${Math.random()+.5})`;
+  el.style.transition = "all 3s";
+  el.style.transform = `translate(${Math.random() * 200 - 100}vw, ${
+    Math.random() * 200 - 100
+  }vh) scale(${Math.random() + 0.5})`;
 }
